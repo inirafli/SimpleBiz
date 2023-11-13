11bfa1903c35a4cf0d9a9503aba3b4fc62d6d836
inirafli <raplirayhann@gmail.com>
2023-11-13 13:44:07 +0700

    responsive hamburger AppBar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,11 @@
 console.log('Hello, SimpleBiz')
+
+// Func buat Hamburger Icon (Sementara)
+document.addEventListener('DOMContentLoaded', () => {
+  const menuIcon = document.querySelector('.menu-icon');
+  const navList = document.querySelector('.nav-list');
+
+  menuIcon.addEventListener('click', () => {
+    navList.classList.toggle('active')
+  })
+})
