eb4fa2de90958dbcd3c4e66a6e50ce0aefe7ef20
inirafli <raplirayhann@gmail.com>
2023-11-16 11:51:28 +0700

    seperated landing page to js and added more responsive element

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,8 @@
+import renderLandingPage from './pages/landingPage'
+
 console.log('Hello, SimpleBiz')
 
-// Func buat Hamburger Icon (Sementara)
 document.addEventListener('DOMContentLoaded', () => {
-  const menuIcon = document.querySelector('.menu-icon');
-  const navList = document.querySelector('.nav-list');
-
-  menuIcon.addEventListener('click', () => {
-    navList.classList.toggle('active')
-  })
+  const appContainer = document.querySelector('#app')
+  renderLandingPage(appContainer)
 })
