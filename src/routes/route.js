158fcbdd4f66fcecb439b710d7853a2602192ba5
77leviann <130739020+77leviann@users.noreply.github.com>
2023-11-29 01:14:24 +0700

    seperated dashboard page to js and added more responsive element

---
+++ src/routes/route.js
@@ -0,0 +1,28 @@
+import renderLandingPage from '../scripts/pages/landingPage'
+import renderRegisterPage from '../scripts/pages/registerPage'
+import renderLoginPage from '../scripts/pages/loginPage'
+import renderDashboardPage from '../scripts/pages/dashboardPage'
+
+const renderPage = () => {
+  const appContainer = document.querySelector('#app')
+  const path = window.location.pathname
+
+  switch (path) {
+    case '/register':
+      renderRegisterPage(appContainer)
+      break
+
+    case '/login':
+      renderLoginPage(appContainer)
+      break
+
+    case '/dashboard':
+      renderDashboardPage(appContainer)
+      break
+
+    default:
+      renderLandingPage(appContainer)
+  }
+}
+
+export default renderPage
