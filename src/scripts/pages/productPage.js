baece43e5ad7a8702b1745e134f8d88cb26496e7
77leviann <130739020+77leviann@users.noreply.github.com>
2023-12-11 10:36:51 +0700

    Update productPage.js

--- src/scripts/pages/productPage.js
+++ src/scripts/pages/productPage.js
@@ -23,12 +23,6 @@ import userIcon from '../../public/icons/user.svg';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyB1FI87qdJUDyHRP8sZTuSbOpfD9Fv8G_E',
-  authDomain: 'simple-biz-app.firebaseapp.com',
-  projectId: 'simple-biz-app',
-  storageBucket: 'simple-biz-app.appspot.com',
-  messagingSenderId: '168574264567',
-  appId: '1:168574264567:web:c3d1105732948875dd5ff2',
 };
 
 // Initialize Firebase
