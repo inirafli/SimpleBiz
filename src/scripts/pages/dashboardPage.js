06365c749d09f90260c66d090b79636e9be81481
77leviann <130739020+77leviann@users.noreply.github.com>
2023-12-11 20:52:59 +0700

    Update dashboardPage.js

--- src/scripts/pages/dashboardPage.js
+++ src/scripts/pages/dashboardPage.js
@@ -25,12 +25,6 @@ import closeIcon from '../../public/icons/close.svg';
 // Import necessary functions from the Firestore module
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB1FI87qdJUDyHRP8sZTuSbOpfD9Fv8G_E',
-  authDomain: 'simple-biz-app.firebaseapp.com',
-  projectId: 'simple-biz-app',
-  storageBucket: 'simple-biz-app.appspot.com',
-  messagingSenderId: '168574264567',
-  appId: '1:168574264567:web:c3d1105732948875dd5ff2',
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
